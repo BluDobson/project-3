6af42562beff09f9c623519dee59442b4b0434a0
Ubuntu <ubuntu@ip-172-31-30-23.eu-west-2.compute.internal>
2021-08-09 14:16:28 +0000

    added script to isntall awscli, eksctl and kubectl on manager node

---
+++ Terraform/ec2/scripts/init.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+sudo apt update
+sudo apt install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"| tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl 
