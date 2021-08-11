da5f4f36a13c15b134f5f4c4524de0f80711535e
ZahraAhmed1 <84967213+ZahraAhmed1@users.noreply.github.com>
2021-08-11 13:46:37 +0100

    Update push_images.sh

--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,1 +1,27 @@
 
+#!/bin/bash
+
+error()
+{
+    echo "[ error]:" "$1" "EXITING" 1>&2
+    exit 1
+}
+
+warning()
+{
+    echo "[WARNING]:" "$1" 1>&2
+}
+
+output()
+{
+    echo "[OUTPUT]:" "$1" 1>&2
+}
+
+output "Pushing qa/petclinic-fe to dockerhub"
+
+if [[ ! "$(docker images -q qa/petclinic-fe:latest 2> /dev/null)" == "" ]]; then
+    output "Image exists"
+    docker push qa/petclinic-fe
+fi 
+
+output "Pushing qa/petclinic-be to dockerhub"
