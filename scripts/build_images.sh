0fb9af2fd7a1afbbbae4d160eb3a27cd930c1f1c
ZahraAhmed1 <84967213+ZahraAhmed1@users.noreply.github.com>
2021-08-11 11:54:22 +0100

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+error()
+{
+    echo "[ error]:" "$1" "EXITING" 1>&2
+    exit 1
+}
+
+warning()
+{
+    echo "[WARNING]:" "$1" 1>&2
+}
+
+output()
+{
+    echo "[OUTPUT]:" "$1" 1>&2
+}
+
+output "Building the Petclinic FE"
+cd ./spring-petclinic-angular/; npm install; npm run-script build; cd ..
+
+output "Building spring-petclinic-angular"
+docker build --no-cache -t (username)/petclinic-fe ./spring-petclinic-angular
+
+if [[ "$(docker images -q (username)/petclinic-fe:latest 2> /dev/null)" == 22]]; then
+    error "could not rebuild image"
+fi 
+
+output "Pushing qa/petclinic-be to dockerhub"
