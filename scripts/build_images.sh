d59a4e03acebb5d00fd021c05b22194fc28df90d
Albert-Richards <84961678+Albert-Richards@users.noreply.github.com>
2021-08-11 15:09:52 +0100

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -16,11 +16,14 @@ output()
     echo "[OUTPUT]:" "$1" 1>&2
 }
 
-output "Building the Petclinic FE"
+output "Building the petclinic backend"
 cd ./Backend/spring-petclinic-rest-master/; npm install; npm run-script build; cd ..
 
 output "Building spring-petclinic-angular"
-docker build --no-cache -t ./Frontend/spring-petclinic-angular-master
+docker build --no-cache -t arichards98/frontend:latest ./Frontend/spring-petclinic-angular-master
+
+output "Building nginx images"
+docker build -t arichards98/nginx:latest -f ./containers/nginx/ .
 
 #if [[ "$(docker images -q arichards98/petclinic-fe:latest 2> /dev/null)" == 22]]; then
 #    error "could not rebuild image"
