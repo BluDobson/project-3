d1da1f913d97f8bb2426345701f3fbdeed892aa4
ZahraAhmed1 <84967213+ZahraAhmed1@users.noreply.github.com>
2021-08-11 11:07:49 +0100

    Update after_installation.sh

--- scripts/after_installation.sh
+++ scripts/after_installation.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+error()
+{
+    echo "[ error]:" "$1" "EXITING" 1>&2
+    exit 1
+}
+
+warning()
+{
+    echo "[WARNING]:" "$1" 1>&2
+}
+
+output()
+{
+    echo "[OUTPUT]:" "$1" 1>&2
+}
+
+output "Removing all dangling images"
+docker system prune -f 
 
