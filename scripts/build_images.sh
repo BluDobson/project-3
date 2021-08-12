2661279097c5439de15ad99aab0b636d6db2ad06
Ubuntu <ubuntu@ip-10-0-0-77.eu-west-2.compute.internal>
2021-08-12 14:51:53 +0000

    added apps, updated scripts

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# ------------------------------------------------------------------
+# Script Name: build_images.sh
+# Description: Building the images for both the spring-petclinic
+#              frontend and backend
+# Author     : QA
+# ------------------------------------------------------------------
 
 error()
 {
-    echo "[ error]:" "$1" "EXITING" 1>&2
+    echo "[ERROR]:" "$1" "EXITING"1>&2
     exit 1
 }
 
@@ -16,17 +22,31 @@ output()
     echo "[OUTPUT]:" "$1" 1>&2
 }
 
-output "Building the petclinic frontend"
-cd ./Frontend/spring-petclinic-angular-master/; npm install; npm run-script build; cd ..
+output "Building the Petclinic FE"
+cd ./spring-petclinic-angular/; npm install; npm run-script build; cd ..
+
+output "Building spring-petclinic-angular"
+docker build --no-cache -t qa/petclinic-fe ./spring-petclinic-angular
+
+if [[ "$(docker images -q qa/petclinic-fe:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
+fi
 
 output "Running maven clean install to rebuild the jar file"
-cd ./Backend/spring-petclinic-rest-master/; mvn clean install; cd ..
+cd ./spring-petclinic-rest/; mvn clean install; cd ..
 
-output "Building images"
-docker-compose up -d ./containers docker-compose.yaml
+output "Building spring-petclinic-rest"
+docker build --no-cache -t qa/petclinic-be ./spring-petclinic-rest
 
-#if [[ "$(docker images -q arichards98/petclinic-fe:latest 2> /dev/null)" == 22]]; then
-#    error "could not rebuild image"
-#fi 
+if [[ "$(docker images -q qa/petclinic-be:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
+fi
 
-output "Pushing qa/petclinic-be to dockerhub"
+output "Building nginx image"
+docker build --no-cache -t qa/nginx ./NGINX
+
+if [[ "$(docker images -q qa/nginx:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
+fi
+
+
