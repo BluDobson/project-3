549194fc111798bec698a5d63ad8ff1aa8dab424
Ubuntu <ubuntu@ip-10-0-0-77.eu-west-2.compute.internal>
2021-08-12 15:01:12 +0000

    updated scripts

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # ------------------------------------------------------------------
-# Script Name: images_push.sh
-# Description: Pushing images for both the spring-petclinic
-#              frontend and backend to docker hub
+# Script Name: build_images.sh
+# Description: Building the images for both the spring-petclinic
+#              frontend and backend
 # Author     : QA
 # ------------------------------------------------------------------
 
@@ -22,24 +22,31 @@ output()
     echo "[OUTPUT]:" "$1" 1>&2
 }
 
-output "arichards98/frontend:latest to dockerhub"
+output "Building the Petclinic FE"
+cd ./spring-petclinic-angular/; npm install; npm run-script build; cd ..
 
-if [[ ! "$(docker images -q arichards98/frontend:latest 2> /dev/null)" == "" ]]; then
-    output "Image exists"
-    docker push arichards98/frontend:latest
+output "Building spring-petclinic-angular"
+docker build --no-cache -t arichards98/frontend:latest ./spring-petclinic-angular
+
+if [[ "$(docker images -q arichards98/frontend:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
 fi
 
-output "Pushing arichards98/backend:latest to dockerhub"
+output "Running maven clean install to rebuild the jar file"
+cd ./spring-petclinic-rest/; mvn clean install; cd ..
 
-if [[ ! "$(docker images -q arichards98/backend:latest 2> /dev/null)" == "" ]]; then
-    output "Image exists"
-    docker push arichards98/backend:latest
+output "Building spring-petclinic-rest"
+docker build --no-cache -t arichards98/backend:latest ./spring-petclinic-rest
+
+if [[ "$(docker images -q arichards98/backend:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
 fi
 
-output "Pushing arichards98/nginx:latest to dockerhub"
+output "Building nginx image"
+docker build --no-cache -t arichards98/nginx:latest ./NGINX
 
-if [[ ! "$(docker images -q arichards98/nginx:latest 2> /dev/null)" == "" ]]; then
-    output "Image exists"
-    docker push arichards98/nginx:latest
+if [[ "$(docker images -q arichards98/nginx:latest 2> /dev/null)" == "" ]]; then
+    error "could not rebuild image"
 fi
+
 
--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,9 +1,14 @@
-
 #!/bin/bash
+# ------------------------------------------------------------------
+# Script Name: images_push.sh
+# Description: Pushing images for both the spring-petclinic
+#              frontend and backend to docker hub
+# Author     : QA
+# ------------------------------------------------------------------
 
 error()
 {
-    echo "[ error]:" "$1" "EXITING" 1>&2
+    echo "[ERROR]:" "$1" "EXITING"1>&2
     exit 1
 }
 
@@ -17,13 +22,24 @@ output()
     echo "[OUTPUT]:" "$1" 1>&2
 }
 
-output "Pushing images to dockerhub"
+output "arichards98/frontend:latest to dockerhub"
 
-docker-compose push ./containers
+if [[ ! "$(docker images -q arichards98/frontend:latest 2> /dev/null)" == "" ]]; then
+    output "Image exists"
+    docker push arichards98/frontend:latest
+fi
 
-#if [[ ! "$(docker images -q qa/petclinic-fe:latest 2> /dev/null)" == "" ]]; then
-#    output "Image exists"
-#    docker push qa/petclinic-fe
-#fi 
+output "Pushing arichards98/backend:latest to dockerhub"
 
-#output "Pushing qa/petclinic-be to dockerhub"
+if [[ ! "$(docker images -q arichards98/backend:latest 2> /dev/null)" == "" ]]; then
+    output "Image exists"
+    docker push arichards98/backend:latest
+fi
+
+output "Pushing arichards98/nginx:latest to dockerhub"
+
+if [[ ! "$(docker images -q arichards98/nginx:latest 2> /dev/null)" == "" ]]; then
+    output "Image exists"
+    docker push arichards98/nginx:latest
+fi
+
