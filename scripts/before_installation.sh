3a88c5923c4caf9b032669307eb1dbcdc0812a0d
Albert-Richards <84961678+Albert-Richards@users.noreply.github.com>
2021-08-12 10:08:03 +0100

    Update before_installation.sh
    added Terraform installation and initialisation

--- scripts/before_installation.sh
+++ scripts/before_installation.sh
@@ -19,6 +19,16 @@ output()
 output "Updating to make sure latest security patches are installed"
 sudo apt update -y
 
+output "Instaling Terraform"
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
+
+output "Initialing Terraform config"
+cd ./Terraform && terraform init
+terraform create
+
 if [[ "$(aws --version 2> /dev/null)" == "" ]]; then 
     error "AWS CLI is not installed"
 fi
